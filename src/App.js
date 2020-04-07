f5928469cf0de2c6bace10850c2a01a4b29b3226
nakkachresta24sowju <nakkachresta24sowju@gmail.com>
2020-04-07 08:30:41 +0000

    final message

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import HomePage from "./components/HomePage";
 import Page1 from "./components/Page1";
-
+import CounterPage from "./components/CounterPage";
 import "./App.css";
 
 const App = () => {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Switch>
+      
+        <Route exact path="/counter-page">
+          <CounterPage />
+        </Route>
         <Route exact path="/page-1">
           <Page1 />
         </Route>
